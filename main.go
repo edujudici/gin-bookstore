682ea3dc30c4291ccc293ce6fad6dc0a29d27eb7
Eduardo Judici <ext.eduardo.judici@dafiti.com.br>
2023-06-11 00:58:17 -0300

    create simple crud

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/edujudici/gin-bookstore/controllers"
+	"github.com/edujudici/gin-bookstore/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
